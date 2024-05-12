ab017cec48248ea3545ba27615f89627ced2ac8b
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 23:22:46 +0300

    Add function draw_swap_plot

--- report_generator.py
+++ report_generator.py
@@ -1,6 +1,7 @@
 import string
 import matplotlib.pyplot as plt
 from pathlib import Path
+from sys import getsizeof
 
 
 def generate_report(benchmark_entries: list):
@@ -8,16 +9,23 @@ def generate_report(benchmark_entries: list):
         print("Nothing to report: no data received")
         return
 
+    print("The benchmark has collected ", getsizeof(benchmark_entries), "bytes of data")
+
     report_time = benchmark_entries[0].date_time.replace(microsecond=0).isoformat()
     directory_name = "Report-{0}".format(report_time)
     path_name = "./generated/{0}".format(directory_name)
     Path(path_name).mkdir(parents=True, exist_ok=True)
 
-    draw_main_plot(benchmark_entries, path_name)
+    draw_plots(benchmark_entries, path_name)
 
 
-def draw_main_plot(benchmark_entries: list, path_name: string):
+def draw_plots(benchmark_entries: list, path_name: string):
     x = [elem.date_time for elem in benchmark_entries]
+    draw_main_plot(benchmark_entries, path_name, x)
+    draw_swap_plot(benchmark_entries, path_name, x)
+
+
+def draw_main_plot(benchmark_entries: list, path_name: string, x):
     y_cpu = [elem.cpu_percentage for elem in benchmark_entries]
     y_ram = [elem.ram_usage.percent for elem in benchmark_entries]
     y_swap = [elem.swap_usage.percent for elem in benchmark_entries]
@@ -39,3 +47,24 @@ def draw_main_plot(benchmark_entries: list, path_name: string):
     ax.legend(handles=[p1, p2, p3])
 
     fig.savefig('{0}/main_plot.png'.format(path_name))
+
+
+def draw_swap_plot(benchmark_entries: list, path_name: string, x):
+    y_swap_total = [elem.swap_usage.total for elem in benchmark_entries]
+    y_swap_used = [elem.swap_usage.used for elem in benchmark_entries]
+
+    (fig, ax) = plt.subplots()
+    twin1 = ax.twinx()
+
+    max_swap_allocated = max(y_swap_total)
+    if max_swap_allocated > 0:
+        ax.set_ylim(0, max_swap_allocated)
+        twin1.set_ylim(0, max_swap_allocated)
+
+    p1, = ax.plot(x, y_swap_total, "b-", label="SWAP Allocated")
+    p2, = twin1.plot(x, y_swap_used, "r-", label="SWAP Usage")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Memory Usage (in %)")
+    ax.legend(handles=[p1, p2])
+    fig.savefig('{0}/swap_plot.png'.format(path_name))
+

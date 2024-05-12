d432d84ad3c4a97dad18105bbe12cf2170fad048
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 03:18:25 +0300

    Move cpu percentage to a variable

--- benchmark.py
+++ benchmark.py
@@ -7,9 +7,10 @@ def generate_report():
 
 
 def collect_usage_data():
+    cpu_percentage = psutil.cpu_percent(interval=1)
     dt = datetime.now()
     print("entry time", dt)
-    print("CPU Usage: ", psutil.cpu_percent(interval=1))
+    print("CPU Usage: ", cpu_percentage)
     vmem = psutil.virtual_memory()
     smem = psutil.swap_memory()
     print("RAM usage", vmem)

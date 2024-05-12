4b788abdbd518db7451e5ed40c567b1fd9010514
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 04:25:17 +0300

    Add ram, swap graphs to end result

--- report_generator.py
+++ report_generator.py
@@ -5,6 +5,8 @@ def generate_report(benchmark_entries: list):
     # y = range(len(benchmark_entries))
     x = [elem.date_time for elem in benchmark_entries]
     y_cpu = [elem.cpu_percentage for elem in benchmark_entries]
+    y_ram = [elem.ram_usage.percent for elem in benchmark_entries]
+    y_swap = [elem.swap_usage.percent for elem in benchmark_entries]
 
     (fig, ax) = plt.subplots()
     twin1 = ax.twinx()
@@ -12,6 +14,13 @@ def generate_report(benchmark_entries: list):
     ax.set_ylim(0, 100)
 
     p1, = ax.plot(x, y_cpu, "b-", label="CPU Load")
+    p2, = twin1.plot(x, y_ram, "r-", label="RAM Usage")
+    p3, = twin2.plot(x, y_swap, "g-", label="SWAP Usage")
+
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Load (in %)")
+
+    ax.legend(handles=[p1, p2, p3])
     fig.savefig('./generated/plot.png')
 
     # for entry in benchmark_entries:

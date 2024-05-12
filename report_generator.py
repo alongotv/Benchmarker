d6094b7327cd1c9b46637f26bf33db64ed493b22
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 04:17:50 +0300

    Add cpu graph to end result

--- report_generator.py
+++ report_generator.py
@@ -2,10 +2,16 @@ import matplotlib.pyplot as plt
 
 
 def generate_report(benchmark_entries: list):
-    y = range(len(benchmark_entries))
+    # y = range(len(benchmark_entries))
     x = [elem.date_time for elem in benchmark_entries]
-    (fig, ax) = plt.subplots(1, 1)
-    ax.plot(x, y)
+    y_cpu = [elem.cpu_percentage for elem in benchmark_entries]
+
+    (fig, ax) = plt.subplots()
+    twin1 = ax.twinx()
+    twin2 = ax.twinx()
+    ax.set_ylim(0, 100)
+
+    p1, = ax.plot(x, y_cpu, "b-", label="CPU Load")
     fig.savefig('./generated/plot.png')
 
     # for entry in benchmark_entries:

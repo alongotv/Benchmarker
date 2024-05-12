efcdc510250a32138347d9dfdabb276a8c648f1e
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 04:06:40 +0300

    Add generated folder to gitignore

--- report_generator.py
+++ report_generator.py
@@ -1,2 +1,11 @@
-def generate_report(benchmark_entries):
-    benchmark_entries
+import matplotlib.pyplot as plt
+
+
+def generate_report(benchmark_entries: list):
+    y = range(len(benchmark_entries))
+    x = [elem.date_time for elem in benchmark_entries]
+    (fig, ax) = plt.subplots(1, 1)
+    ax.plot(x, y)
+    fig.savefig('./generated/plot.png')
+
+    # for entry in benchmark_entries:

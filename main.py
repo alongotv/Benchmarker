4ff608f4836b07cb4c7581be5d1d219558e64b18
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 03:47:09 +0300

    Generate report after benchmark

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import benchmark
+import report_generator
 import time
+
+benchmark.system_info()
+
+benchmark_entries = list()
 
 try:
     while True:
         bench_entry = benchmark.collect_usage_data()
+        benchmark_entries.add(bench_entry)
         print(bench_entry)
         time.sleep(1)
 except KeyboardInterrupt:
     pass
 
-
-benchmark.generate_report()
+report_generator.generate_report(benchmark_entries)

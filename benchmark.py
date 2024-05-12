d781803b80aa0e91727cd899ed42df792a6b7026
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-12 03:32:09 +0300

    Add class BenchEntry

--- benchmark.py
+++ benchmark.py
@@ -7,11 +7,24 @@ def generate_report():
 
 
 def collect_usage_data():
-    cpu_percentage = psutil.cpu_percent(interval=1)
+    cpu_percentage = psutil.cpu_percent()
     dt = datetime.now()
-    print("entry time", dt)
-    print("CPU Usage: ", cpu_percentage)
     vmem = psutil.virtual_memory()
     smem = psutil.swap_memory()
-    print("RAM usage", vmem)
-    print("SWAP Usage: ", smem)
+    return BenchEntry(date_time=dt, cpu_percentage=cpu_percentage, ram_usage=vmem, swap_usage=smem)
+
+
+class BenchEntry:
+    date_time = None
+    cpu_percentage = None
+    ram_usage = None
+    swap_usage = None
+
+    def __init__(self, date_time, cpu_percentage, ram_usage, swap_usage):
+        self.date_time = date_time
+        self.cpu_percentage = cpu_percentage
+        self.ram_usage = ram_usage
+        self.swap_usage = swap_usage
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
